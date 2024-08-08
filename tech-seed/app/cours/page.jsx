640ecb447b9e97f2062ffe792b88483b63ce9463
BrunelDev <ahokpossibrunel@gmail.com>
2024-08-08 10:21:34 +0100

    a commit

---
+++ tech-seed/app/cours/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "@/components/navbar";
+export default function Course() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="bg-gradient-to-r from-[#FFF5BE] from-100% to-[#D0F7EA] to-100%">
+        <img src="schoolSentence.svg" alt="" />
+        <div></div>
+      </div>
+    </>
+  );
+}
--- tech-seed/components/button.jsx
+++ tech-seed/components/button.jsx
@@ -12,7 +12,7 @@ export default function Button({
   console.log(title);
   return (
     <span
-      className={`${textColor} hover:${hoverTextColor} hover:${bgColor} py-2 px-3 text-center rounded-md ${bgColor} w-fit h-fit cursor-pointer`}
+      className={`${textColor} hover:${hoverTextColor} hover:${bgColor} py-2 px-3 text-center rounded-md ${bgColor} w-fit h-fit cursor-pointer z-50`}
       onClick={() => {
         action();
       }}
