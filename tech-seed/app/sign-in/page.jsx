f2c11fec37f1a7914ede707db3f42cd8ed8ba3ad
orou18 <orouloence@gmail.com.com>
2024-08-09 13:42:54 +0200

    Sign-up

--- tech-seed/app/sign-in/page.jsx
+++ tech-seed/app/sign-in/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Connexion} from '@/components/connexion.tsx';
 
-export default function Signup() {
+export default function Sign_in() {
   return (
     <div>
         <Connexion/>
