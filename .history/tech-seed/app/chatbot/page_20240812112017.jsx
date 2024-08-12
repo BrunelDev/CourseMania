68b4b9e4f058027638f347b217861b8510b4ddc2
BrunelDev <ahokpossibrunel@gmail.com>
2024-08-12 16:12:46 +0100

    a commit

---
+++ .history/tech-seed/app/chatbot/page_20240812112017.jsx
@@ -0,0 +1,59 @@
+"use client";
+import { useState } from "react";
+import { CircleX } from "lucide-react";
+import { ChatInput } from "@/components/chatInput";
+import { PreparedMesssage } from "@/components/preparedMesssage";
+import { MessageList } from "react-chat-elements";
+import "react-chat-elements/dist/main.css";
+
+export default function page() {
+  const [message, setMessage] = useState("");
+  return (
+    <div className="relative w-[400px] h-[600px] bg-green-300">
+      <header className="bg-[#20B486] flex justify-between items-center">
+        <img src="wappGPTlogo2.png" alt="" />
+        <div className="flex flex-col">
+          <span>WappGPT</span>
+          <span>Online</span>
+        </div>
+        <CircleX color="#ffffff" />
+      </header>
+      <div className="w-full">
+        <MessageList
+          className="message-list"
+          lockable={true}
+          toBottomHeight={"10%"}
+          dataSource={[
+            {
+              position: "left",
+              type: "text",
+              title: "Kursat",
+              text: "Give me a message list example !",
+            },
+            {
+              position: "right",
+              type: "text",
+              title: "Emre",
+              text: "That's all.",
+            },
+          ]}
+        />
+      </div>
+      <div className="absolute w-full px-2 bottom-0">
+        <div className="flex space-x-2 mb-1">
+          <PreparedMesssage
+            value={"Qu'est-ce que Iziskill ?🤔"}
+            setMessage={setMessage}
+          />
+          <PreparedMesssage value={"FAQ's"} setMessage={setMessage} />
+          <PreparedMesssage
+            value={"Qu'est-ce que WappGPT ? 🤔"}
+            setMessage={setMessage}
+          />
+        </div>
+
+        <ChatInput value={message} setValue={setMessage} />
+      </div>
+    </div>
+  );
+}
---
+++ .history/tech-seed/components/preparedMesssage_20240812104039.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function PreparedMesssage({ value }) {
+  return (
+    <span className="bg-[#F3F5F6] text-center cursor-pointer p-2">{value}</span>
+  );
+}
