060c2d0dcef27c4dfa38a91b7a61564cea928ba4
orou18 <orouloence@gmail.com.com>
2024-08-14 16:48:12 +0200

    help

--- tech-seed/components/Nav.jsx
+++ tech-seed/components/Nav.jsx
@@ -4,11 +4,11 @@ export const Nav = () => {
   return (
     <div className="flex w-full fixed top-0 left-0 items-center justify-between py-6 px-12 backdrop-blur-md z-50">
     <ul className="flex space-x-4 text-[#101828] font-medium">
-      <li className="text-[#1A906B] font-semibold">Accueil</li>
-      <li>A propos</li>
-      <li>Cours</li>
-      <li>Contact</li>
-      <li>FAQs</li>
+      <a href=""><li className="text-[#1A906B] font-semibold">Accueil</li></a>
+      <a href=""><li>A propos</li></a>
+      <a href=""><li>Cours</li></a>
+      <a href=""><li>Contact</li></a>
+      <a href=""><li>FAQs</li></a>
     </ul>
   </div>
   )
