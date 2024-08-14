0dc78127222974c1a7f181ae11316a47f3556133
orou18 <orouloence@gmail.com.com>
2024-08-14 10:27:29 +0200

    modif

--- tech-seed/app/Reviews/page.jsx
+++ tech-seed/app/Reviews/page.jsx
@@ -1,37 +1,13 @@
+"use client"
 import React from "react";
+import Reviews from "@/components/reviews";
 import Lateralenavmentor from "@/components/lateralenavmentor";
-import { CarouselDemo } from "@/components/carousselComment";
-export default function Reviews() {
-    return (
-        <div className="inline-block">
-            <div className="flex flex-col">
-                <div className="bg-[aliceblue] ml-[10px] w-[1430px] h-80">
-                        <div className="ml-32 mt-9">
-                            <img src="image 1.png" alt="" />
-                        </div>
-                        <div className="ml-56 -mt-16">
-                            <h1 className="text-lg font-bold">Titre de la videoo</h1>
-                            <span className="flex flex-row">
-                                5.0
-                                <img src="start.png" alt="" />
-                                <img src="start.png" alt="" />
-                                <img src="start.png" alt="" />
-                                <img src="start.png" alt="" />
-                                <img src="start.png" alt="" />
-                            </span>
-                            <p className="ml-80 -mt-4 text-gray-400">notation de ce cours</p>
-                            <br />
-                        <div className="flex flex-row justify-between mt-40 w-[800px] ">
-                            <img src="start.png" alt="" />
-                            <img src="start.png" alt="" className="" />
-                            </div>
-                        </div>
-                </div>
-                <div className="ml-[50px] mt-20">
-                    <CarouselDemo
 
-                    />
-                </div>
+export default function Commentaire() {
+    return (
+        <div className="flex flex-row">
+            <div className="w-80">
+                <Reviews />
             </div>
         </div>
     )
